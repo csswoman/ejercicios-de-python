0b4a380db882ba60ff6553bfa2924614bbd025c3
Karla Agraz <karla.agraz@gmail.com>
2020-07-14 18:05:02 -0500

    condicionales

--- conversor-sol-dolar.py
+++ conversor-sol-dolar.py
@@ -1,3 +1,17 @@
+menu = """
+Bienvenido al conversor de monedas 💰
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+if opcion == '1':
+
+
 sol = input (" ¿Cuántos soles peruanos tienes?: ")
 sol = float(sol)
 valor_dolar = 3.50
