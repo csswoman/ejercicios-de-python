55ca4d6e79a51e29ec4c08967d7f042a6f42e548
Karla Agraz <karla.agraz@gmail.com>
2020-07-14 17:31:54 -0500

    conversor de dolar a soles

--- conversor.py
+++
@@ -1,7 +0,0 @@
-pesos = input (" ¿Cuántos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dólares")
