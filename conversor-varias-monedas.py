28b8031cb81756194925a5e53d4649c2ac938c30
Karla Agraz <karla.agraz@gmail.com>
2020-07-14 18:55:36 -0500

    conversor de varias monedas

--- conversor-varias-monedas.py
+++ conversor-varias-monedas.py
@@ -10,10 +10,28 @@ Elige una opción: """
 opcion = int(input(menu))
 
 if opcion == 1:
-    pass
+    pesos = input("¿Cuántos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
 elif opcion == 2:
-    pass
+    pesos = input("¿Cuántos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
 elif opcion == 3:
-    pass
+    pesos = input("¿Cuántos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
 else:
     print('Ingresa una opción correcta, por favor')
