bd8b2c905d6711f64c23aa1e01d765a08dae3b39
Karla Agraz <karla.agraz@gmail.com>
2020-07-18 01:18:48 -0500

    ciclo for

---
+++ for.py
@@ -0,0 +1,13 @@
+# contador = 1
+# print(contador)
+# while contador < 1000:
+#     contador += 1
+#     print(contador)
+
+
+# for contador in range (1, 1001):
+#     print(contador)
+
+# Tabla de multiplicar del 11
+for i in range(10):
+    print(11 * i)
